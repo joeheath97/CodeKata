7bbb8e779e4bc97057f7445a7a063326c1ff9899
Joe Heath <heath_j09@outlook.com>
2020-08-10 23:08:29 +0100

    1.0.0

---
+++ VowelCount/VowelCount/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace VowelCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            vowelCount("Hello World!");
+
+        }
+
+        public static int vowelCount(string sentence)
+        {
+            var charactersArray = sentence.ToCharArray();
+
+            var vowelRegex = new Regex("[a,e,i,o,u]");
+
+            var count = 0;
+
+            foreach(var c in charactersArray)
+            {
+                if (vowelRegex.IsMatch(c.ToString()) == true)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("[{0}]", string.Join("," , charactersArray));
+
+            Console.WriteLine(count);
+
+            return count;
+        } 
+    }
+}
