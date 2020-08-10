e4f0b278ed2f91933625b99b481e17e3b12d2a43
Joe Heath <heath_j09@outlook.com>
2020-08-10 23:06:24 +0100

    1.0.0

---
+++ Who's_Next_in_Line/Who's_Next_in_Line/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Who_s_Next_in_Line
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] queue = { "Sally", "Jim", "Bob", "Kate" };
+            Console.WriteLine(NextPersonInQueue(queue, 3));
+        }
+
+        public static string NextPersonInQueue(string[] queue, int position)
+        {
+
+            return queue[position - 1];
+        }
+    }
+}
