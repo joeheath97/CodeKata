7950e722ed477808a4667b2b44cabb2eb43f6f3c
Joe Heath <heath_j09@outlook.com>
2020-08-10 23:11:01 +0100

    1.0.0

---
+++ Multiples3n5/Multiples3n5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Multiples3n5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(totalMuliples(10));
+        }
+
+        public static int totalMuliples(int value)
+        {
+
+            return Enumerable.Range(0, value).Where(n => n % 3 == 0 || n % 5 == 0).Sum();
+        }
+    }
+}
