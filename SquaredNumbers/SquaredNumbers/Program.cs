9733d747b9ae9f8f5e888dbf3de85a44c15ac979
Joe Heath <heath_j09@outlook.com>
2020-09-06 17:29:15 +0100

    1.0.0

---
+++ SquaredNumbers/SquaredNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace SquaredNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SquaredNumber(9119));
+        }
+
+        public static int SquaredNumber(int n)
+        {
+
+            // LINQ : splits string into char array and converts them to doubles
+            // Char needs to be coverted to string inorder to be converted to double
+            var digets = n.ToString().Select(x => double.Parse(x.ToString()));
+
+            var result = "";
+
+            /*
+             * loops through char array 
+             * 
+             * number to the power of
+             * 
+             * and then toString to concate
+             * 
+             **/
+            foreach(var i in digets)
+            {
+                var squardedNumber = Math.Pow(i, 2);
+                result += squardedNumber.ToString();
+            }
+
+            Console.WriteLine(int.Parse(result));
+
+            return int.Parse(result);
+        }
+    }
+}
