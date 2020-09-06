0ac1e031a7420b893487f1f1b56d8add33728801
Joe Heath <heath_j09@outlook.com>
2020-09-06 19:57:27 +0100

    1.0.0

---
+++ CreatePhoneNumber/CreatePhoneNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace CreatePhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CreatePhoneNumber(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
+        }
+
+        public static string CreatePhoneNumber(int [] numbers)
+        {          
+            return string.Format("({0}{1}{2}) {3}{4}{5} - {6}{7}{8}{9}", numbers.Select(x => x.ToString()).ToArray());
+        }
+    }
+}
