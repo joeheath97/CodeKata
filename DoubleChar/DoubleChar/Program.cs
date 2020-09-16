dc3e37cd4d8ae134c67d0fe6d4b3f9226d40ac79
Joe Heath <heath_j09@outlook.com>
2020-09-16 18:25:39 +0100

    1.0.0

---
+++ DoubleChar/DoubleChar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DoubleChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DoubleChar("Hello World!"));
+        }
+
+        public static string DoubleChar(string s)
+        {
+            var doubleChar = new List<char>();
+
+
+            foreach (char c in s)
+            {
+                doubleChar.Add(c);
+                doubleChar.Add(c);
+            }
+
+            return String.Join("", doubleChar);
+        }
+    }
+}
